441616ee94c334a53a4449d254f4baf0188b684f
bbest <bbarani@gmail.com>
2018-10-26 09:19:12 -0700

    Update routes.js

--- common/routes.js
+++ common/routes.js
@@ -3,16 +3,7 @@ import {Switch, Route} from 'react-router-dom';
 import Index from './containers/Index';
 import Image from './containers/Images';
 import NotFound from './containers/NotFound';
-/**
- * Defines the constructor for the router
- * Used for both server side & client side rendering
- * switch statement for routing to different routes
- * phones context root to use Phones component
- * tablets context root to use Tablets component
- * prepaid context root to use Prepaid component
- * sim context root to use Sim component
- * @constructor
- */
+
 const Routes = () => (
     <Switch>
         <Route path="/index" component={Index}/>
